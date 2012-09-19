cbdfb363d8d09e315b60f565389adb651cddd60b
ilake <lake.ilakela+github@gmail.com>
2012-09-19 17:32:05 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KhanTranslater::Application.config.secret_token = '771032bb265d89abd09e5c9e5777499acad017f262230177047ea77fe4add8d73cb9bcc97a5d6b794149c03ac0000dcb28daee7f26fd7e60f302f518f3a77ddd'
